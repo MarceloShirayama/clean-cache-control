f34778b3e7df69e4997d58c47f1df6caaa55be8b
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2023-01-22 17:19:10 -0300

    ensure LocalLoadPurchases not delete or insert cache on sut.init

---
+++ tests/data/use-cases/local-load-purchases.spec.ts
@@ -0,0 +1,24 @@
+import { describe, expect, it } from "vitest";
+
+import { LocalLoadPurchases } from "@/data/use-cases";
+import { CacheStoreSpy } from "tests/mocks";
+
+type SutTypes = {
+  sut: LocalLoadPurchases;
+  cacheStore: CacheStoreSpy;
+};
+
+const makeSut = (timestamp = new Date()): SutTypes => {
+  const cacheStore = new CacheStoreSpy();
+  const sut = new LocalLoadPurchases(cacheStore, timestamp);
+
+  return { cacheStore, sut };
+};
+
+describe("LocalLoadPurchases", () => {
+  it("Should not delete or insert cache on sut.init", () => {
+    const { cacheStore } = makeSut();
+
+    expect(cacheStore.actions).toEqual([]);
+  });
+});
