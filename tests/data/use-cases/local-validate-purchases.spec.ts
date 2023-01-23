259d8e92f95a8dda1bae0b4de0b2b86127c3903f
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2023-01-22 21:02:47 -0300

    ensure LocalLoadPurchases deletes cache on validate if cache expired

--- tests/data/use-cases/local-validate-purchases.spec.ts
+++ tests/data/use-cases/local-validate-purchases.spec.ts
@@ -1,7 +1,11 @@
 import { describe, expect, it } from "vitest";
 
 import { LocalLoadPurchases } from "@/data/use-cases";
-import { CacheStoreSpy, getCacheExpirationDate } from "tests/mocks";
+import {
+  CacheStoreSpy,
+  getCacheExpirationDate,
+  mockPurchases,
+} from "tests/mocks";
 
 type SutTypes = {
   sut: LocalLoadPurchases;
@@ -49,5 +53,42 @@ describe("LocalValidatePurchases", () => {
 
     expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.fetch]);
     expect(cacheStore.fetchKey).toBe("purchases");
+  });
+
+  it(`Should delete cache if cache expired`, () => {
+    const currentDate = new Date();
+    const timestamp = getCacheExpirationDate(currentDate);
+    timestamp.setSeconds(timestamp.getSeconds() - 1);
+
+    const { cacheStore, sut } = makeSut(currentDate);
+
+    cacheStore.fetchResult = { timestamp };
+
+    sut.validate();
+
+    expect(cacheStore.actions).toEqual([
+      CacheStoreSpy.Action.fetch,
+      CacheStoreSpy.Action.delete,
+    ]);
+    expect(cacheStore.fetchKey).toBe("purchases");
+    expect(cacheStore.deleteKey).toBe("purchases");
+  });
+
+  it(`Should delete cache if cache on expiration date`, () => {
+    const currentDate = new Date();
+    const timestamp = getCacheExpirationDate(currentDate);
+
+    const { cacheStore, sut } = makeSut(currentDate);
+
+    cacheStore.fetchResult = { timestamp };
+
+    sut.validate();
+
+    expect(cacheStore.actions).toEqual([
+      CacheStoreSpy.Action.fetch,
+      CacheStoreSpy.Action.delete,
+    ]);
+    expect(cacheStore.fetchKey).toBe("purchases");
+    expect(cacheStore.deleteKey).toBe("purchases");
   });
 });
